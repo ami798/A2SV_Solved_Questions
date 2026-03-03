2c39ac3e04ca58391938886d2e4679ae95b2d435
Amira Abdurahman <amiraabdurahman8@gmail.com>
2026-03-03 16:37:07 +0300

    Time: 132 ms (37.51%), Space: 33.3 MB (84.55%) - LeetHub

---
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -0,0 +1,32 @@
+class NumMatrix:
+#prefix[i][j] = matrix[i][j] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1]
+    def __init__(self, matrix):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        self.prefix = [[0]*(n+1) for _ in range(m+1)]
+
+        for r in range(1, m+1):
+            for c in range(1, n+1):
+                self.prefix[r][c] = (
+                    matrix[r-1][c-1]
+                    + self.prefix[r-1][c]
+                    + self.prefix[r][c-1]
+                    - self.prefix[r-1][c-1]
+                )
+
+    def sumRegion(self, row1, col1, row2, col2):
+
+        row1 += 1
+        col1 += 1
+        row2 += 1
+        col2 += 1
+
+        return (
+            self.prefix[row2][col2]
+            - self.prefix[row1-1][col2]
+            - self.prefix[row2][col1-1]
+            + self.prefix[row1-1][col1-1]
+        )
+
+ 
