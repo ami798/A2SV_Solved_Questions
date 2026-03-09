a154f71ee42f1765e02a688dc24827c845aa7ce9
Amira Abdurahman <amiraabdurahman8@gmail.com>
2026-03-09 17:59:19 +0300

    Add solution for Valid Parentheses

---
+++ leetcode/0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+       
+        for i in s:       
+            if i in "{,(,[":   
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                if i == ')':
+                    if stack[-1] == '(':
+                        stack.pop()
+                    else:
+                        return False
+                elif i == ']':
+                    if stack[-1] == '[':
+                        stack.pop()
+                    else:
+                        return False
+                elif i == '}':
+                    if stack[-1] == '{':
+                        stack.pop()
+                    else:
+                        return False
+        
+        
+        return len(stack)==0
+
+
+        
