135e185a12a97b4c665b14f6cfb391dff364ac66
Amira Abdurahman <amiraabdurahman8@gmail.com>
2026-03-21 22:34:23 +0300

    Time: 48 ms (95.14%), Space: 33.2 MB (62.5%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
         fre = Counter(nums)
-        for key, count in fre.items():# by using hash mapping just like di)
+        for key, count in fre.items():
             if count * 2 > len(nums):
                 dominant = key
                 total_count = count
@@ -16,3 +16,5 @@ class Solution:
             if left_count * 2 > i + 1 and (total_count - left_count) * 2 > len(nums) - (i + 1):
                 return i
         return -1
+
+        
