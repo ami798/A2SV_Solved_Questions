45d53e3211b8bcb91edb08eaa20bab3849224f17
Amira Abdurahman <amiraabdurahman8@gmail.com>
2026-04-08 01:08:41 +0300

    Add solution for Happy Number

---
+++ leetcode/0202-happy-number/0202-happy-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def get_next(num):
+            total = 0
+            while num > 0:
+                digit = num % 10
+                total += digit * digit
+                num //= 10
+            return total
+        
+        seen = set()
+        
+        while n != 1:
+            if n in seen:
+                return False
+            seen.add(n)
+            n = get_next(n)
+        
+        return True
