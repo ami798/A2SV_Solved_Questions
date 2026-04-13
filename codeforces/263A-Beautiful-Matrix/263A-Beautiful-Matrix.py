21223a2b4576451620a83552a038a9d78b8b95b0
Amira Abdurahman <amiraabdurahman8@gmail.com>
2026-04-14 00:16:27 +0300

    Add solution for Beautiful Matrix

---
+++ codeforces/263A-Beautiful-Matrix/263A-Beautiful-Matrix.py
@@ -0,0 +1,14 @@
+matrix = []
+
+for i in range(5):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j] == 1:
+            r, c = i, j
+r += 1
+c += 1
+
+print(abs(r - 3) + abs(c - 3))
