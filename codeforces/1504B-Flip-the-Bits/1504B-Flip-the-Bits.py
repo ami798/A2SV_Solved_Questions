668963757652074f24b84128ad2b1eb068878279
Amira Abdurahman <amiraabdurahman8@gmail.com>
2026-04-17 22:33:18 +0300

    Add solution for Flip the Bits

---
+++ codeforces/1504B-Flip-the-Bits/1504B-Flip-the-Bits.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = input().strip()
+        b = input().strip()
+        balance = [0] * n
+        ones = zeros = 0
+
+        for i in range(n):
+            if a[i] == '1':
+                ones += 1
+            else:
+                zeros += 1
+            balance[i] = ones - zeros
+
+        flipped = False
+        possible = True
+        for i in range(n - 1, -1, -1):
+            current = a[i]
+            if flipped:
+                current = '1' if current == '0' else '0'
+            if current != b[i]:
+                if balance[i] != 0:
+                    possible = False
+                    break
+                flipped = not flipped  
+
+        print("YES" if possible else "NO")
+
+
+solve()
