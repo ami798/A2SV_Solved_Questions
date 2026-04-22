764cc014a525cb5dd08e102889dffefe4bbccfe5
Amira Abdurahman <amiraabdurahman8@gmail.com>
2026-04-22 19:17:14 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -4,12 +4,18 @@ class Solution:
             return []
 
         rows, cols = len(heights), len(heights[0])
-
-        pacific = set()
-        atlantic = set()
+        res = []
 
         def dfs(r, c, visited):
+            
+            if (r, c) in visited:
+                return (False, False)
+
             visited.add((r, c))
+
+            
+            pacific = (r == 0 or c == 0)
+            atlantic = (r == rows - 1 or c == cols - 1)
 
             directions = [(1,0), (-1,0), (0,1), (0,-1)]
 
@@ -17,27 +23,19 @@ class Solution:
                 nr, nc = r + dr, c + dc
 
                 if (0 <= nr < rows and 0 <= nc < cols and
-                    (nr, nc) not in visited and
-                    heights[nr][nc] >= heights[r][c]):
-                    
-                    dfs(nr, nc, visited)
+                    heights[nr][nc] <= heights[r][c]):  
 
-        
-        for c in range(cols):
-            dfs(0, c, pacific)
-        for r in range(rows):
-            dfs(r, 0, pacific)
+                    p, a = dfs(nr, nc, visited)
+                    pacific = pacific or p
+                    atlantic = atlantic or a
 
-        
-        for c in range(cols):
-            dfs(rows - 1, c, atlantic)
-        for r in range(rows):
-            dfs(r, cols - 1, atlantic)
+            return (pacific, atlantic)
 
-        res = []
         for r in range(rows):
             for c in range(cols):
-                if (r, c) in pacific and (r, c) in atlantic:
+                visited = set()
+                p, a = dfs(r, c, visited)
+                if p and a:
                     res.append([r, c])
 
         return res
